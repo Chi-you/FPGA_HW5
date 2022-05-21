eec21c28f2e00a487381612bf61c3f37a77d33bf
Chi-you <yoyolee789@gmail.com>
2022-05-22 02:13:06 +0800

    Fix indentation

--- src/hdl/ref/mult.v
+++ src/hdl/ref/mult.v
@@ -5,14 +5,14 @@ module mult ( // 25 * 19 => 25 * 18 +
     output [47:0] POUT1
 );
 
-assign POUT1 = A*B;
+    assign POUT1 = A*B;
 
-wire [42:0] POUT2 = {{18{A[24]}}, A[24:0]} * {{25{B[18]}}, B[18:1]};
-wire [47:0] tmp1 = {{5{POUT2[42]}}, POUT2};
+    wire [42:0] POUT2 = {{18{A[24]}}, A[24:0]} * {{25{B[18]}}, B[18:1]};
+    wire [47:0] tmp1 = {{5{POUT2[42]}}, POUT2};
 
-wire fabric1 = A[0] & B[0];
-wire [23:0] fabric2 = B[0] ? A[24:1] : 0;
+    wire fabric1 = A[0] & B[0];
+    wire [23:0] fabric2 = B[0] ? A[24:1] : 0;
 
-wire [46:0] tmp2 = tmp1[46:0] + {{23{fabric2[23]}}, fabric2};
-assign POUT0 = {tmp2, fabric1};
+    wire [46:0] tmp2 = tmp1[46:0] + {{23{fabric2[23]}}, fabric2};
+    assign POUT0 = {tmp2, fabric1};
 endmodule
